cdcea93384f8c9ec20823f9c00b011f1340b78f2
Nooorrrr <noor.bnhdd@gmail.com>
2024-03-18 15:58:53 +0100

    setting up necessary files

---
+++ lib/pages/start_screen.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DashApp'),
+      ),
+      body: const Center(
+        child: Text('Welcome to DashApp'),
+      ),
+    );
+  }
+}
